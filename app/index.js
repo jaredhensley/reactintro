8c5b9c1a8947791f61278b498ebfd48d3ebaab12
Jared Hensley <jahensle85@me.com>
2016-10-14 23:49:46 -0500

    initial commit

---
+++ app/index.js
@@ -0,0 +1,27 @@
+
+
+// var Hello = React.createClass({
+//   render: function () {
+//     return (
+//       <div>Hello ReactJS Program!</div>
+//     )
+//   }
+// });
+//
+// ReactDOM.render(<Hello />, document.getElementById('app'));
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ var HelloWorld = React.CreateClass({
+   render: function() {
+     return {
+       <div>Hello World</div>
+     }
+   }
+ });
+
+ ReactDOM.render {
+   <HelloWorld />,
+   document.getElementById('app');
+ }
