0f512daa3dc42b3c6d8abe243ea86b34864fe7f3
Jared Hensley <jahensle85@me.com>
2016-10-15 23:51:02 -0500

    project1

--- app/index.js
+++ app/index.js
@@ -13,15 +13,16 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
- var HelloWorld = React.CreateClass({
-   render: function() {
-     return {
-       <div>Hello World</div>
-     }
-   }
- });
+var Hello = React.createClass({
+  render: function () {
+    console.log(this.props);
+    return (
+      <div>
+        <div> {this.props.age} </div>
+        <div> {this.props.name} </div>
+      </div>
+    )
+  }
+});
 
- ReactDOM.render {
-   <HelloWorld />,
-   document.getElementById('app');
- }
+ReactDOM.render(<Hello name="jared" age={30}/>, document.getElementById('app'));
